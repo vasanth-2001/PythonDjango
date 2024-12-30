76df5688a6c17a684e289ad765e29e946078c563
vasanth-2001 <vasanth2001ms@gmail.com>
2024-12-30 18:27:59 +0530

    Initial commit

---
+++ mysite/author/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def index(request):
+    return HttpResponse("hello Everyone")
+# Create your views here.
+def index(request):
+    return HttpResponse("hello Everyone")
+def index(request):
+    return HttpResponse("hello Everyone")
+def index(request):
+    return HttpResponse("hello Everyone")
