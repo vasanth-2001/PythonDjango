21ed7e2d624ae557e4c362cc447504f3e6f527ad
vasanth-2001 <vasanth2001ms@gmail.com>
2024-12-31 17:00:35 +0530

    Django

--- mysite/author/views.py
+++ mysite/author/views.py
@@ -1,12 +1,12 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+# from django.shortcuts import render
+# from django.http import HttpResponse
 
-def index(request):
-    return HttpResponse("hello Everyone")
-# Create your views here.
-def index(request):
-    return HttpResponse("hello Everyone")
-def index(request):
-    return HttpResponse("hello Everyone")
-def index(request):
-    return HttpResponse("hello Everyone")
+# def index(request):
+#     return HttpResponse("hello Everyone")
+# # Create your views here.
+# def index(request):
+#     return HttpResponse("hello Everyone")
+# def index(request):
+#     return HttpResponse("hello Everyone")
+# def index(request):
+#     return HttpResponse("hello Everyone")
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -19,5 +19,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('myblog/', include('author.urls')),
+    # path('myblog/', include('author.urls')),
 ]
